2437f601c2634dbf4df98de3ac3c6737b46fb7fd
rahmman <rahmman.cse@outlook.com>
2017-03-12 22:15:44 -0500

    first commit

---
+++ angularHttpService1/angularHttpService1/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace angularHttpService1
+{
+    internal class Employee
+    {
+        public int id
+        {
+            get; set;
+        }
+        public string name
+        {
+            get; set;
+        }
+        public string gender
+        {
+            get; set;
+        }
+        public int salary
+        {
+            get; set;
+        }
+    }
+}
---
+++ angularHttpService1/angularHttpService1/Script/Script.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular
+.module("myModule", [])
+.controller("myController", function ($scope, $http) {
+    $http.post('EmployeeService.asmx/GetAllEmployees')
+    .then(function (response) {
+        $scope.employees = response.data;
+    });
+});
